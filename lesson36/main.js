af1a426800998b343ad263b2fa6d022eeff26554
Alextter <74066029+Alextter@users.noreply.github.com>
2021-03-01 19:42:20 +0200

    Add files via upload

---
+++ lesson36/main.js
@@ -0,0 +1,45 @@
+// let x = prompt("Vvedite chislo")
+// if ( x==10){
+//     alert("Verno")
+// }
+// else{
+//     alert("NE VERNO")
+// }
+
+// let x = prompt("Какая четверть")
+
+// if (x > 0 && x < 15){
+//     alert("В первой")
+// }
+// else if (x >= 15 && x <30 ) {
+//     alert("В четвертой")
+// }
+// else if (x >=30 && x <45 ){
+//     alert("В третьей")
+// }
+// else if (x >= 45 && x < 60){
+//     alert("Во второй")
+// }
+
+
+
+
+// let z= prompt("Ввдеите язык")
+// let x = arr = [ "Пн","вт" ,"ср","чт","пн"]
+
+// let y = arr = [ "pn","vt" ,"sr","cht","pn"]
+// if( z == "ru" ){
+//     alert(x)
+// }
+// else{
+//     alert(y)
+// }
+
+let a =Number (prompt(" Введите число а"))
+b = Number (prompt("Введите число б"))
+if( a <= 1  && b >= 3){
+    alert( a + b)
+}
+else {
+    alert(a-b)
+}
